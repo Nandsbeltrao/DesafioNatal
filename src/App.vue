8f048010bb4f0fa5f50f60f5531c7182679b9865
Fernanda <febeltraoleite@gmail.com>
2024-12-11 00:39:34 -0300

    Feat : Adiciona os Componentes da Página Footer

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Inicio from './components/Inicio.vue';
 import Cronometro from './components/Cronometro.vue';
 import Doacao from './components/Doacao.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Inicio />
   <Cronometro />
   <Doacao />
+  <Footer />
 </template>
 
 <style scoped>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,44 @@
 <script setup>
+    import PapaiNoel from "/papaiNoel.png";
+    import redesSociais from "/redesSociais.png";
 </script>
 
 <template>
+    <footer>
+        <div>
+            <img :src="PapaiNoel" alt="Ilustração do Papai Noel">
+            <h2>Feliz Natal</h2>
+        </div>
 
+        <img class="redes" :src="redesSociais" alt="">
+    </footer>
 </template>
 
 <style scoped lang="scss">
+    footer{
+        background-color: #FFE9E8;
+        display: flex;
+        justify-content: space-around;
+        height: 8vh;
+
+        div{
+
+            img{
+                margin-top: 1.2rem;
+            }
+            h2{
+                margin-left: 3rem;
+                margin-top: -2rem;
+                font-family: "Archivo", sans-serif;
+                font-size: 1.2rem;
+                font-weight: 500;
+            }
+        }
+
+        .redes{
+            width: 15vw;
+            height: 3.8vh;
+            margin-top: 1rem;
+        }
+    }
 </style>
